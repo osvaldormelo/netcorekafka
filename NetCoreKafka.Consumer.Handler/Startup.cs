b107c7559232888f66e2d66db5dd183741372a87
osvaldormelo <osvaldormelo@gmail.com>
2021-06-15 00:10:41 -0300

    Ajusters no consumer

--- NetCoreKafka.Consumer.Handler/Startup.cs
+++ NetCoreKafka.Consumer.Handler/Startup.cs
@@ -18,7 +18,7 @@ namespace Kafka.Consumer.Handler
         {
             services.AddSingleton<IHostedService, MessageHandler>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
